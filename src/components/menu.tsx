1d030dbd061dc4bcf1479b49350def6149d3619e
Noah Paessel <knowuh@gmail.com>
2019-10-23 14:58:29 -0400

    getting started with the menu component

---
+++ src/components/menu.tsx
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+
+interface IMenuItemProps {
+  text: string
+  selected?: boolean
+  disabled?: boolean
+  link?: string
+  onClick?: (source: IMenuItemProps) => void
+}
+
+interface IMenuProps {
+  title: string
+  items: IMenuItemProps[]
+}
+
+const MenuItem: React.FC<IMenuItemProps> = (props: IMenuItemProps) => {
+  const {text, selected, link, onClick} = props
+  return (
+    <div className='menuItem' key={text}>{text}</div>
+  )
+}
+
+const MenuButton: React.FC<IMenuProps> = (props: IMenuProps) => {
+  const [opened, toggleOpen] = useState(false)
+  const toggle = () => toggleOpen(!opened)
+  const {title, items} = props;
+  return (
+    <div className="menu">
+      <div className="menuButton" onClick={toggle}> {title} </div>
+      { opened &&
+        <div className="menuItems">
+          { items.map(i => <MenuItem {...i}/>) }
+        </div>
+      }
+    </div>
+  );
+}
+
+MenuButton.defaultProps = {
+  title: 'Untitled Menu',
+  items: [
+    {text: 'a item'},
+    {text: 'b item'},
+    {text: 'c item'},
+    {text: 'd item'}
+  ]
+}
+
+export default MenuButton;
---
+++ src/stories/2-menu.stories.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Menu from '../components/menu'
+export default {
+  title: 'Clue Menu'
+}
+
+export const basic = () => <Menu />
+export const defaultProps = () => <Menu />
