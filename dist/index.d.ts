594280f4b2d47b186a9a17c3404343840dd5845a
Noah Paessel <knowuh@gmail.com>
2019-11-07 14:45:36 -0500

    Remove the now ignored directory node_modules

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { DropDown, IMenuItemProps } from './components/drop-down';
-import { ToggleGroup, IToggleGroupProps, IToggleChoice } from './components/toggle-group';
-import { Colors } from './themes/clue';
-export { DropDown, IMenuItemProps, ToggleGroup, IToggleGroupProps, IToggleChoice, Colors };
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-import { DropDown } from './components/drop-down';
-import { ToggleGroup } from './components/toggle-group';
-import { Colors } from './themes/clue';
-export { DropDown, ToggleGroup, Colors };
