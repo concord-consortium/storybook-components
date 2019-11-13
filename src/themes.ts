26c43e796fe31593c1d541fa863d31264b3da1c8
Noah Paessel <knowuh@gmail.com>
2019-11-13 13:39:26 -0500

    Add default theme, bump  version.

--- src/themes.ts
+++ src/themes.ts
@@ -1,4 +1,4 @@
 import { Clue } from "./themes/clue";
 import { Dataflow } from "./themes/dataflow";
 
-export const Themes = { Clue, Dataflow };
+export const Themes = { Clue, Dataflow, Default: Clue };
