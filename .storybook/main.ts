7e6cb84082d3be327c283e82bb2a505610b957d3
Scott Cytacki <scytacki@concord.org>
2024-03-13 16:49:57 -0400

    support React 18
    I tested this by bumping the version of React in package.json to 18. And everything worked. It printed warnings about unknown properties, which was due to a change in styled components. The fix is to prefix properties for styled built in dom elements with $. styled-components calls these transient properties.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,6 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-webpack5-compiler-swc",
-    "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
